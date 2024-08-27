4586277baaf880b03eeb51200de6e5513161d2c2
Jarvy <jarvy@physiotec.org>
2024-08-27 10:52:45 -0400

    Add documentation to PersonViewController

--- src/main/java/com/champlain/oop2a1/PersonViewController.java
+++ src/main/java/com/champlain/oop2a1/PersonViewController.java
@@ -6,18 +6,32 @@ import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.paint.Color;
 
+/**
+ * The PersonViewController class is the controller for the UI that allows
+ * users to interact with a Person object. It handles actions such as saving
+ * a person's data, loading example data, and purchasing a parking pass.
+ */
 public class PersonViewController {
+
     @FXML
     private Label aParkingPassLabel;
+
     @FXML
     private TextField aNameTextField;
+
     @FXML
     private TextField aPhoneNumberTextField;
+
     @FXML
     private TextField aEmailAddressTextField;
 
     private Person aPerson;
 
+    /**
+     * Handles the save button click event. This method saves the data entered
+     * into the text fields into a Person object. If the data is valid, it displays
+     * the person's details and shows a confirmation alert.
+     */
     @FXML
     protected void onSaveButtonClick() {
         // Making sure there is a Person object to work with.
@@ -38,6 +52,10 @@ public class PersonViewController {
         successAlert.showAndWait();
     }
 
+    /**
+     * Handles the load example button click event. This method loads an example
+     * person's data into the text fields and displays the data.
+     */
     @FXML
     protected void onLoadExampleButtonClick() {
         this.aPerson = new Person();
@@ -52,6 +70,11 @@ public class PersonViewController {
         this.displayPerson(this.aPerson);
     }
 
+    /**
+     * Handles the buy parking pass button click event. This method changes the
+     * parking pass status of the person and displays the updated information.
+     * If the person already has a parking pass, an error alert is shown.
+     */
     @FXML
     protected void onBuyPassButtonClick() {
         // Making sure there is a Person object to work with.
@@ -68,6 +91,12 @@ public class PersonViewController {
         }
     }
 
+    /**
+     * Displays the details of the person in the corresponding text fields and updates
+     * the parking pass label based on the person's parking pass status.
+     *
+     * @param person the person whose details are to be displayed.
+     */
     private void displayPerson(Person person) {
         aNameTextField.setText(aPerson.getName());
         aPhoneNumberTextField.setText(aPerson.getPhoneNumber());
@@ -81,6 +110,11 @@ public class PersonViewController {
         }
     }
 
+    /**
+     * Handles input errors by displaying an error alert with the corresponding message.
+     *
+     * @param e the exception thrown when input data is invalid.
+     */
     private void handleInputError(IllegalArgumentException e) {
         Alert errorAlert = new Alert(Alert.AlertType.ERROR, "Entered data invalid: " + e.getMessage());
         errorAlert.showAndWait();
