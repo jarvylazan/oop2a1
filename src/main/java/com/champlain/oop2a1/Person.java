899e971e53d741186703b0cf3b41934bfda7c306
Jarvy <jarvy@physiotec.org>
2024-08-28 11:02:24 -0400

    Create validation method for email address.

--- src/main/java/com/champlain/oop2a1/Person.java
+++ src/main/java/com/champlain/oop2a1/Person.java
@@ -27,15 +27,19 @@ public class Person {
     /**
      * Parameterized constructor for the Person class.
      *
-     * @param pName        the name of the person.
+     * @param pName         the name of the person.
      * @param pEmailAddress the email address of the person.
-     * @param pPhoneNumber the phone number of the person.
-     * @param pPassStatus  the parking pass status of the person.
+     * @param pPhoneNumber  the phone number of the person.
+     * @param pPassStatus   the parking pass status of the person.
      */
     public Person(String pName, String pEmailAddress, String pPhoneNumber, boolean pPassStatus) {
         aName = pName;
-        aEmailAddress = pEmailAddress;
         aPhoneNumber = pPhoneNumber;
+        if (!pEmailAddress.isEmpty()) {
+        validateEmailAddress(pEmailAddress);
+        }
+        // Assign the email nonetheless, so it will be shown in the textbox.
+        aEmailAddress = pEmailAddress;
         aPassStatus = pPassStatus;
     }
 
@@ -52,14 +56,12 @@ public class Person {
      * Sets the email address of the person.
      * Validates the email address format before setting it.
      *
-     * @param pEmailAddress the email address to set.   
+     * @param pEmailAddress the email address to set.
      * @throws IllegalArgumentException if the email address is not in a valid format.
      */
     public void setEmailAddress(String pEmailAddress) {
-        if (pEmailAddress.matches("^[\\w-.]+@([\\w-]+\\.)+[\\w-]{2,4}$")) {
+        if (pEmailAddress.isEmpty() || validateEmailAddress(pEmailAddress)) {
             aEmailAddress = pEmailAddress;
-        } else {
-            throw new IllegalArgumentException("\"" + pEmailAddress + "\" is not a valid email address.");
         }
     }
 
@@ -78,7 +80,11 @@ public class Person {
      * @return true after the parking pass is purchased.
      */
     public boolean PurchaseParkingPass() {
+        if (!aPassStatus) {
         return aPassStatus = true;
+        } else {
+            return false;
+        }
     }
 
     /**
@@ -115,5 +121,21 @@ public class Person {
      */
     public String getEmailAddress() {
         return aEmailAddress;
+    }
+
+    /**
+     * Checks if the email address passed into the class is valid and
+     * throws an exception if it is not.
+     *
+     * @param pEmailAddress the email address to validate.
+     * @throws IllegalArgumentException if the email address is invalid
+     */
+
+    private boolean validateEmailAddress(String pEmailAddress) {
+        if (pEmailAddress.matches("^[\\w-.]+@([\\w-]+\\.)+[\\w-]{2,4}$")) {
+            return true;
+        } else {
+            throw new IllegalArgumentException("\"" + pEmailAddress + "\" is not a valid email address.");
+        }
     }
 }
